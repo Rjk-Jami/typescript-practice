e6ea6a570c9f9e8f472f3b78f24947bab953b937
Rjk-Jami <rjkhan.1221@gmail.com>
2025-02-17 13:16:38 +0600

    access modifier

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var myNewFunc = function (a, b, c) {
     console.log("hello ".concat(a, " ").concat(b));
 };
-//  kisu define na korle void return korbe,  ar javascript e return kore unDefined 
+//  kisu define na korle void return korbe,  ar javascript e return kore unDefined
 //  void mane akbare sunno, = kisu nai
 // unDefined mane, defined na kintu akta value
 myNewFunc("a", "b");
@@ -37,3 +37,34 @@ var players = [];
 players.push(jami);
 players.push(himel);
 console.log(players);
+//  access modifier
+var Player2 = /** @class */ (function () {
+    function Player2(n, a, c) {
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    }
+    Player2.prototype.play = function () {
+        console.log("".concat(this.name, " from ").concat(this.country, " is playing!"));
+    };
+    return Player2;
+}());
+var jami1 = new Player("jami", 25, "bogura");
+var himel1 = new Player("himel", 25, "kosba");
+var players2 = [];
+players.push(jami1);
+players.push(himel1);
+console.log(players2);
+//  jodi access modifier define kore dei
+// access modifier na dile, this use kore constructor er modde define kore dite hobe
+var Player3 = /** @class */ (function () {
+    function Player3(name, age, country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
+    Player3.prototype.play = function () {
+        console.log("".concat(this.name, " from ").concat(this.country, " is playing!"));
+    };
+    return Player3;
+}());
--- script.ts
+++ script.ts
@@ -1,59 +1,100 @@
 // type Function
-let myNewFunc = (a:string, b:string, c?:string)=>{
-    console.log(`hello ${a} ${b}`)
-}
-//  kisu define na korle void return korbe,  ar javascript e return kore unDefined 
+let myNewFunc = (a: string, b: string, c?: string) => {
+  console.log(`hello ${a} ${b}`);
+};
+//  kisu define na korle void return korbe,  ar javascript e return kore unDefined
 //  void mane akbare sunno, = kisu nai
 // unDefined mane, defined na kintu akta value
-myNewFunc("a", "b")
-let myNewFunc2 = (a:string, b:string, c:string = "null")=>{
-    console.log(`hello ${a} ${b}`)
-}
+myNewFunc("a", "b");
+let myNewFunc2 = (a: string, b: string, c: string = "null") => {
+  console.log(`hello ${a} ${b}`);
+};
 
-myNewFunc2("a", "b")
-
+myNewFunc2("a", "b");
 
 // type aliases = nickname er moto, same type er data akdik jaigai babohar korle, code er simplicity er jonno common vabe declare kore dile eke type aliases bole
 
 //  Function Signature
-let userDetails : (id: number | string, userInfo:{
-    name: string,
-    age: number
-}) => void
+let userDetails: (
+  id: number | string,
+  userInfo: {
+    name: string;
+    age: number;
+  }
+) => void;
 
-userDetails = (id: number | string, info:{
-    name:string,
-    age:number
-}) =>{
-console.log(`id: ${id}, name: ${info.name}, age: ${info.age} `)
-}
+userDetails = (
+  id: number | string,
+  info: {
+    name: string;
+    age: number;
+  }
+) => {
+  console.log(`id: ${id}, name: ${info.name}, age: ${info.age} `);
+};
 
-userDetails("jami1", {name:"jami khan", age:25})
-
+userDetails("jami1", { name: "jami khan", age: 25 });
 
 //  class type
 //  class hoilo object er blueprints
 class Player {
-    name : string;
-    age: number;
-    country : string
-    constructor(n:string, a:number, c:string){
-        this.name = n
-        this.age = a
-        this.country = c
-    }
-    play(){
-        console.log(`${this.name} from ${this.country} is playing!`)
-    }
+  name: string;
+  age: number;
+  country: string;
+  constructor(n: string, a: number, c: string) {
+    this.name = n;
+    this.age = a;
+    this.country = c;
+  }
+  play() {
+    console.log(`${this.name} from ${this.country} is playing!`);
+  }
 }
 
-const jami = new Player("jami", 25, "bogura")
-const himel = new Player("himel", 25,"kosba")
+const jami = new Player("jami", 25, "bogura");
+const himel = new Player("himel", 25, "kosba");
 
-let players: Player[] = []
+let players: Player[] = [];
 
-players.push(jami)
-players.push(himel)
+players.push(jami);
+players.push(himel);
 
-console.log(players)
- 
+console.log(players);
+
+//  access modifier
+class Player2 {
+  private name: string; //not reedit from another space | class er baire use kora jabe na
+  public age: number;
+  readonly country: string; // readonly private er motoi , eita baire theke change kora jabe na kintu view kora jabe
+  constructor(n: string, a: number, c: string) {
+    this.name = n;
+    this.age = a;
+    this.country = c;
+  }
+  play() {
+    console.log(`${this.name} from ${this.country} is playing!`);
+  }
+}
+
+const jami1 = new Player("jami", 25, "bogura");
+const himel1 = new Player("himel", 25, "kosba");
+
+let players2: Player2[] = [];
+
+players.push(jami1);
+players.push(himel1);
+
+console.log(players2);
+
+//  jodi access modifier define kore dei
+// access modifier na dile, this use kore constructor er modde define kore dite hobe
+class Player3 {
+  constructor(
+    private name: string,
+    public age: number,
+    readonly country: string
+  ) {}
+  play() {
+    console.log(`${this.name} from ${this.country} is playing!`);
+  }
+}
